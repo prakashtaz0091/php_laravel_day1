ea3c27231d4591077acb64601462a8c374412fa8
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-28 12:17:06 +0545

    another way i.e. view()->with(data) to pass data to view

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/services/{service_name}/{opt_param?}', function ($service_name, $op
     // echo 'service_detail   ' . $service_name . '   ' . $opt_param;
 
     $data = compact('service_name', 'opt_param'); // compact('key', 'value') it is same as array('key' => 'value'), simply converting datas into array
-    return view('service', $data);  // passing data to view
+    // return view('service', $data);  // passing data to view
+    return view('service')->with($data);  //another way of passing data to view
 });
 
