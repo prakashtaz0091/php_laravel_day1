adbcce97196db5330890908d03fad6cd043e64c6
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-28 12:41:00 +0545

    handling safe and unsafe data in blade template

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,3 +1,10 @@
 <h1>{{ $service_name}}</h1>
 
 <p>{{ $opt_param  ?? "default opt params"  }}</p>
+
+{{-- here {{ $unsafe_data}}  this syntax automatically escapes the unsafe data --}}
+{{ $unsafe_data }}  
+
+
+{{-- here {!! $unsafe_data !!} this syntax doesn't protects from unsafe data --}}
+{!! $unsafe_data !!}
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,17 @@ Route::get('/services', function () {
 Route::get('/services/{service_name}/{opt_param?}', function ($service_name, $opt_param = null) {
     // echo 'service_detail   ' . $service_name . '   ' . $opt_param;
 
-    $data = compact('service_name', 'opt_param'); // compact('key', 'value') it is same as array('key' => 'value'), simply converting datas into array
+    $unsafe_data = "<script>alert('Unsafe data');</script>";
+
+    $data = compact('service_name', 'opt_param', 'unsafe_data'); // compact('key', 'value') it is same as array('key' => 'value'), simply converting datas into array
     // return view('service', $data);  // passing data to view
+    
+    
+    
     return view('service')->with($data);  //another way of passing data to view
+
+
+
+
 });
 
