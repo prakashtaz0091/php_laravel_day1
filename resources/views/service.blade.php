22062d6abfb5c861abb4658e498435e8eff62987
prakashtaz0091 <code.ing2468@gmail.com>
2024-05-28 12:44:20 +0545

    if else in blade template

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,10 +1,16 @@
-<h1>{{ $service_name}}</h1>
+<h1>{{ $service_name }}</h1>
 
-<p>{{ $opt_param  ?? "default opt params"  }}</p>
+<p>{{ $opt_param ?? 'default opt params' }}</p>
+
+@if ($opt_param)
+    {{ $opt_param }}
+@else
+    <p>no opt param</p>
+@endif
 
 {{-- here {{ $unsafe_data}}  this syntax automatically escapes the unsafe data --}}
-{{ $unsafe_data }}  
+{{ $unsafe_data }}
 
 
 {{-- here {!! $unsafe_data !!} this syntax doesn't protects from unsafe data --}}
-{!! $unsafe_data !!}
+{{-- {!! $unsafe_data !!} --}}
